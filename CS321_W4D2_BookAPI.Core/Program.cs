cc5642ee4883a4795443b7f393508a99abb5f357
ck1962 <ckeslin@gmail.com>
2019-08-11 14:43:46 -0500

    initial create

---
+++ CS321_W4D2_BookAPI.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS321_W4D2_BookAPI.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
